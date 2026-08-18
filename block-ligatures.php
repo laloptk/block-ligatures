937493e4282352131993dba36cbe1abc0a169590
Eduardo Sanchez <laloptk@users.noreply.github.com>
2026-08-18 13:50:00 -0600

    Initial commit

---
+++ block-ligatures.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name:       Block Ligatures
+ * Description:       Adds ligature support to block editor typography.
+ * Version:           1.0.0
+ * Requires at least: 6.0
+ * Requires PHP:      7.4
+ * Author:            Lalo
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       block-ligatures
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+define( 'BLOCK_LIGATURES_VERSION', '1.0.0' );
+define( 'BLOCK_LIGATURES_PATH', plugin_dir_path( __FILE__ ) );
+define( 'BLOCK_LIGATURES_URL', plugin_dir_url( __FILE__ ) );
+
+require_once BLOCK_LIGATURES_PATH . 'vendor/autoload.php';
